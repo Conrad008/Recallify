8960ad44db120686c0eb4a7ae2e77ed82c614d07
Conrad008 <kipngeno7180@gmail.com>
2026-08-10 12:10:53 +0300

    feat: : add database initialization and session retriever helper

--- database.py
+++ database.py
@@ -8,3 +8,12 @@ engine = create_engine(DATABASE_URL, echo=False)
 Base = declarative_base()
  
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+def init_db():
+
+    import models  
+    Base.metadata.create_all(bind=engine)
+
+def get_session():
+
+    return SessionLocal()
