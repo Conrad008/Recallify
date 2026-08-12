e1cca952001790fedde6b72d786e828e67ad7e36
Conrad008 <kipngeno7180@gmail.com>
2026-08-12 12:22:17 +0300

    feat: implement build_header with user greeting and action buttons

--- deck.py
+++ deck.py
@@ -7,5 +7,29 @@ class DeckListView(ctk.CTkFrame):
         super().__init__(master)
         self.master_app = master
  
-        self._build_header()
-        self._build_deck_list()
+        self.build_header()
+        self.build_deck_list()
+
+     
+    def build_header(self):
+        header = ctk.CTkFrame(self, fg_color="transparent")
+        header.pack(fill="x", padx=24, pady=(20, 10))
+ 
+        welcome = ctk.CTkLabel(
+            header,
+            text=f"Welcome, {self.master_app.current_user.username}",
+            font=ctk.CTkFont(size=22, weight="bold"),
+        )
+        welcome.pack(side="left")
+ 
+        logout_btn = ctk.CTkButton(
+            header, text="Log Out", width=90, fg_color="#555555",
+            command=self.master_app.logout,
+        )
+        logout_btn.pack(side="right")
+ 
+        new_deck_btn = ctk.CTkButton(
+            header, text="+ New Deck", width=120, command=self._open_new_deck_dialog
+        )
+        new_deck_btn.pack(side="right", padx=(0, 10))
+    
