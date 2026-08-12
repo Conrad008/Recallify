2a3b3cca07d2b4e48702e7f78f4ad71ef10a61bf
Conrad008 <kipngeno7180@gmail.com>
2026-08-13 02:26:17 +0300

    fix: change indentation of code block

--- app.py
+++ app.py
@@ -24,41 +24,42 @@ class App(ctk.CTk):
         self.current_frame = None
         self.show_login()
 
-def show_login(self):
-    self._switch(LoginView(self, on_login_success=self.on_login_success))
+    def show_login(self):
+        self._switch(LoginView(self, on_login_success=self.on_login_success))
  
-def on_login_success(self, user):
-    self.current_user = user
-    self.show_deck_list()
+    def on_login_success(self, user):
+        self.current_user = user
+        self.show_deck_list()
  
-def show_deck_list(self):
-    self._switch(DeckListView(self))
+    def show_deck_list(self):
+        self._switch(DeckListView(self))
  
-def show_deck_detail(self, deck_id: int):
-    from deckDetail import DeckDetail
-    self._switch(DeckDetail(self, deck_id))
+    def show_deck_detail(self, deck_id: int):
+        from deckDetail import DeckDetail
+        self._switch(DeckDetail(self, deck_id))
  
-def show_review(self, deck_id: int):
-    from review import ReviewView
-    self._switch(ReviewView(self, deck_id))
- 
-def show_stats(self, deck_id: int = None):
-    from ui.stats_view import StatsView
-    self._switch(StatsView(self, deck_id))
- 
-def logout(self):
-    self.current_user = None
-    self.show_login()
- 
-def _switch(self, frame: ctk.CTkFrame):
-    if self.current_frame is not None:
-        self.current_frame.destroy()
-    self.current_frame = frame
-    self.current_frame.pack(fill="both", expand=True)
- 
-def on_closing(self):
-    self.session.close()
-    self.destroy()
+    def show_review(self, deck_id: int):
+        from review import ReviewView
+        self._switch(ReviewView(self, deck_id))
+
+    # def show_stats(self, deck_id: int = None):
+    #     from ui.stats_view import StatsView
+    #     self._switch(StatsView(self, deck_id))
+    
+    def logout(self):
+        self.current_user = None
+        self.show_login()
+        
+    def _switch(self, frame: ctk.CTkFrame):
+        if self.current_frame is not None:
+            self.current_frame.destroy()
+
+        self.current_frame = frame
+        self.current_frame.pack(fill="both", expand=True)
+            
+    def on_closing(self):
+        self.session.close()
+        self.destroy()
 
 def run():
     app = App()
