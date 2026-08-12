b00f5c75e1eb19ca001f323255cafdfbbc8895f2
Conrad008 <kipngeno7180@gmail.com>
2026-08-12 10:15:24 +0300

    feat: initialize LoginView frame structure and mode state

--- login.py
+++ login.py
@@ -1,5 +1,13 @@
 import customtkinter as ctk
  
 from auth import login_user, register_user, AuthError
+
+class LoginView(ctk.CTkFrame):
+    def __init__(self, master, on_login_success):
+        super().__init__(master)
+        self.master_app = master
+        self.on_login_success = on_login_success
  
+        self.mode = "login"  
  
+        self._build_ui()
